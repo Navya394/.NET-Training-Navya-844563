f1a86a74ad398ad0c0c506d8c0c17070cc0a752f
Navya394 <60810955+Navya394@users.noreply.github.com>
2020-02-18 15:39:08 +0530

    UserInformation

---
+++ New folder/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NAVYAHANDSON.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index( string input1,int input2)
+        {
+            NAVYAEntities2 obj = new NAVYAEntities2();
+            var userid = from i in obj.USERINFOes where (i.USERNAME == input1) && (i.PASSWD == input2) select i.USERID;
+            var uid = userid.FirstOrDefault();
+            if(uid!=0)
+            {
+                var data = from i in obj.USERDETAILS where (i.USERID == uid) select i;
+                var name = data.FirstOrDefault();
+                ViewBag.name = name.NAME;
+                ViewBag.age = name.AGE;
+                ViewBag.city = name.CITY;
+                ViewBag.reportingto = name.REPORTINGTTO;
+                return View();
+   }
+           else
+            {
+                return View("failed login");
+            }
+        }
+    }
+}
